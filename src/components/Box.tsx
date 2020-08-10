f7e0b5597a41d829fac546ccce93e14c9a83ce83
yingjieweb <1184061078@qq.com>
2020-08-10 14:10:58 +0800

    test styled-components use

---
+++ src/components/Box.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Box = styled.div`
+  border: 1px solid red;
+  min-height: 300px;
+  min-width: 300px;
+  margin: 20px;
+`
+export default Box
---
+++ src/components/Button.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  color: red;
+  border: none;
+  background: orange;
+  &:hover { background: yellow };
+`
+export default Button
