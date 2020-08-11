407829d8ba384f703104ccffe374ad6cb26d27a2
yingjieweb <1184061078@qq.com>
2020-08-11 16:53:18 +0800

    set up four component modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,58 @@
 import React from 'react';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
 function App() {
   return (
-      <div>111</div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/money">记账页</Link>
+              </li>
+              <li>
+                <Link to="/labels">标签页</Link>
+              </li>
+              <li>
+                <Link to="/statistics">统计页</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Redirect exact from="/" to="/money"></Redirect>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/labels">
+              <Labels />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
+}
+
+function Money() {
+  return <h2>记账页</h2>;
+}
+
+function Labels() {
+  return <h2>标签页</h2>;
+}
+
+function Statistics() {
+  return <h2>统计页</h2>;
 }
 
 export default App;
