b84816a9bdab0b82a62f30170818b38307e143e3
yingjieweb <1184061078@qq.com>
2020-08-12 10:26:45 +0800

    Nav add icons

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,6 +2,12 @@ import styled from "styled-components";
 import {Link} from "react-router-dom";
 import React from "react";
 
+import money from 'assets/icons/money.svg'
+import label from 'assets/icons/label.svg'
+import statistics from 'assets/icons/statistics.svg'
+
+console.log(money, label, statistics)
+
 const NavContent = styled.div`
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -20,12 +26,21 @@ const Nav = () => {
       <NavContent>
         <ul>
           <li>
+            <svg fill="red" className="icon">
+              <use xlinkHref="#money"></use>
+            </svg>
             <Link to="/money">记账页</Link>
           </li>
           <li>
+            <svg fill="red" className="icon">
+              <use xlinkHref="#label"></use>
+            </svg>
             <Link to="/labels">标签页</Link>
           </li>
           <li>
+            <svg fill="red" className="icon">
+              <use xlinkHref="#statistics"></use>
+            </svg>
             <Link to="/statistics">统计页</Link>
           </li>
         </ul>
