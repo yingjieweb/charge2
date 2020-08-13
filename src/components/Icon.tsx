78e041e4426c0f7ab8ca50fb377aa25b975a5d5a
yingjieweb <1184061078@qq.com>
2020-08-13 10:05:53 +0800

    batch import svg icons

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 require('assets/icons/money.svg')
 require('assets/icons/label.svg');
 require('assets/icons/statistics.svg');
+//直接引入/assets/icons目录下的所有svg图片，
+const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
 
 type Props = {
     name: string;
