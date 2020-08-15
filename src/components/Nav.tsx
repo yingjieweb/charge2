36264b312afcae638798b3290825038e69e25f42
yingjieweb <1184061078@qq.com>
2020-08-15 10:52:57 +0800

    adjust Nav icon styles can be click

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,18 +7,20 @@ const NavContent = styled.div`
   line-height: 24px;
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   > ul {
-    display: flex;
+    display:flex;
     > li {
       width: 33.3333%;
-      padding: 4px 0;
-      text-align: center;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      .icon {
-        width: 24px;
-        height: 24px;
+      text-align:center;
+      > a {
+        display: flex;
+        flex-direction: column;
+        padding: 4px 0;
+        justify-content: center;
+        align-items: center;
+        .icon {
+          width: 24px;      
+          height: 24px;
+        }
       }
     }
   }
@@ -29,16 +31,22 @@ const Nav = () => {
       <NavContent>
         <ul>
           <li>
-            <Icon name="money"></Icon>
-            <Link to="/money">记账页</Link>
+            <Link to="/money">
+              <Icon name="money"></Icon>
+              记账页
+            </Link>
           </li>
           <li>
-            <Icon name="label"></Icon>
-            <Link to="/labels">标签页</Link>
+            <Link to="/labels">
+              <Icon name="label"></Icon>
+              标签页
+            </Link>
           </li>
           <li>
-            <Icon name="statistics"></Icon>
-            <Link to="/statistics">统计页</Link>
+            <Link to="/statistics">
+              <Icon name="statistics"></Icon>
+              统计页
+            </Link>
           </li>
         </ul>
       </NavContent>
