339b1e4c574e794cf52459a8a4466c7ddb063a7f
yingjieweb <1184061078@qq.com>
2020-08-17 16:53:15 +0800

    basic implement NumberPad component's function

--- src/components/money/NumberPadSection.tsx
+++ src/components/money/NumberPadSection.tsx
@@ -1,6 +1,72 @@
 import styled from 'styled-components';
+import React, {useState} from "react";
 
-const NumberPadSection = styled.section`
+const NumberPadSection:React.FC = () => {
+  const [output, setOutput] = useState('0');
+  const onClickNumberPad = (event: React.MouseEvent) => {
+    let text = (event.target as HTMLButtonElement).textContent;
+    if (text === null) return
+    switch (text) {
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+        if (output === '0') {
+          setOutput(text);
+        } else {
+          setOutput(output + text);
+        }
+      case '.':
+        if (output.indexOf('.') >= 0) {return output;}
+        setOutput(output + '.');
+      case '删除':
+        if (output.length === 1) {
+          setOutput('0');
+        } else {
+          setOutput(output.slice(0, -1) || '0');
+        }
+      case '清空':
+        setOutput('0');
+      case 'OK':
+        setOutput('OK');
+      default:
+        setOutput('0');
+    }
+  }
+  return (
+      <Wrapper>
+        <div className="output">
+          {output}￥
+        </div>
+        <div className="pad clearfix" onClick={onClickNumberPad}>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>删除</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>清空</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button className="ok">OK</button>
+          <button className="zero">0</button>
+          <button className="dot">.</button>
+        </div>
+      </Wrapper>
+  )
+}
+
+export {NumberPadSection};
+
+const Wrapper = styled.section`
   display:flex;
   flex-direction: column;
   > .output{
@@ -14,7 +80,12 @@ const NumberPadSection = styled.section`
   }
   > .pad{ 
     > button{
-      font-size: 18px; float: left; width: 25%; height: 50px; border: none;
+      font-size: 18px;
+      float: left;
+      width: 25%;
+      height: 50px;
+      border: none;
+      outline: none;
       &.ok{ height: 100px; float: right; }
       &.zero{ width: 50%; }
       &:nth-child(1){
@@ -48,6 +119,3 @@ const NumberPadSection = styled.section`
     }
   }
 `;
-
-
-export {NumberPadSection};
--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -11,27 +11,7 @@ function Money() {
         <TagsSection></TagsSection>
         <NoteSection></NoteSection>
         <CategorySection></CategorySection>
-        <NumberPadSection>
-          <div className="output">
-            0￥
-          </div>
-          <div className="pad clearfix">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>删除</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>清空</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button className="ok">OK</button>
-            <button className="zero">0</button>
-            <button className="dot">.</button>
-          </div>
-        </NumberPadSection>
+        <NumberPadSection></NumberPadSection>
       </Layout>
   )
 }
