33dedfe05d34bd8c144f4e480fb43acc2b9f4aab
yingjieweb <1184061078@qq.com>
2020-08-24 15:30:42 +0800

    finish TagEdit's save function

--- src/scripts/useTags.tsx
+++ src/scripts/useTags.tsx
@@ -2,8 +2,27 @@ import {useState} from "react";
 
 const useTags = () => {
   const [tags, setTags] = useState<string[]>(['餐饮', '购物', '交通', '娱乐', '工资']);
-  const findTag = (tagParams:string) => {return tags.filter(tag => tag === tagParams)}
-  return {tags, setTags, findTag};
+  const findTag = (tagParams:string) => {return tags.filter(tag => tag === tagParams)[0]};
+  const updateTag = (matchedTag:string, newTag:string) => {
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    let matchedTagIndex = tagsClone.indexOf(matchedTag);
+    tagsClone.splice(matchedTagIndex, 1);
+    tagsClone.push(newTag);
+    setTags(tagsClone);
+  };
+  const deleteTag = () => {
+    console.log('delete');
+  }
+  const saveTag = () => {
+    const tagsClone = JSON.parse(JSON.stringify(tags));
+    let emptyStrIndex = tagsClone.indexOf('');
+    tagsClone.splice(emptyStrIndex, 1);
+    setTags(tagsClone);
+    alert('保存成功！');
+    window.history.back();
+
+  }
+  return {tags, setTags, findTag, updateTag, saveTag, deleteTag};
 }
 
 export {useTags}
--- src/views/TagEdit.tsx
+++ src/views/TagEdit.tsx
@@ -1,29 +1,45 @@
-import React from "react";
+import React, {ChangeEventHandler} from "react";
 import {useTags} from "../scripts/useTags";
 import {useParams} from "react-router-dom"
 import Layout from "../components/Layout";
 import { Button } from "components/Button";
 import {Input} from "../components/Input";
+import styled from "styled-components";
 
 type Params = {
   routerTag: string;
 }
 
-const TagEdit:React.FC = () => {
-  const {findTag} = useTags();
+const TagEdit:React.FC = (props) => {
+  const {findTag, updateTag,saveTag, deleteTag} = useTags();
   let {routerTag} = useParams<Params>();
   const matchedTag = findTag(routerTag);
+  const onChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    updateTag(matchedTag, event.target.value);
+  };
   return (
       <Layout title='编辑标签' leftIconName='left'>
-        <div>
-          <Input></Input>
-        </div>
-        <div>
-          <Button>保存</Button>
-          <Button>删除</Button>
-        </div>
+        <Wrapper>
+          <Input label="标签名" type="text" placeholder="请输入新的标签名" defaultValue={matchedTag} onChange={onChange}></Input>
+        </Wrapper>
+        <ButtonsWrapper>
+          <Button onClick={() => {saveTag()}}>保存</Button>
+          <Button onClick={() => {deleteTag()}}>删除</Button>
+        </ButtonsWrapper>
       </Layout>
   )
 }
+export {TagEdit};
 
-export {TagEdit}
+const Wrapper = styled.section`
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+`;
+
+const ButtonsWrapper = styled.div`
+  margin-top: 28px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`
