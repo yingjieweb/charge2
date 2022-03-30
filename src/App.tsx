219cc5ffaa41ec0030a60b2952199579e19de6e0
yingjieweb <1184061078@qq.com>
2022-03-30 23:25:22 +0800

    feat: basic finish router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {HashRouter, Routes, Route, Navigate, Link} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      嘀嘀嘀
-    </div>
+      <HashRouter>
+        <Link to="/money">money</Link>
+        <Link to="/labels">labels</Link>
+        <Routes>
+          <Route path="/" element={<Navigate to="/money" replace/>}/>
+          <Route path="/money" element={'money'}/>
+          <Route path="/labels" element={'labels'}/>
+          <Route path="/tag/:routerTag" element={'tag'}/>
+          <Route path="/statistics" element={'statistics'}/>
+          <Route path="*" element={'404'}/>
+        </Routes>
+      </HashRouter>
   );
 }
 
