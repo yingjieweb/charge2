7f42aab3a3f3d7ffda4304a2f7ac2b48b3113c90
yingjieweb <1184061078@qq.com>
2022-04-10 23:28:10 +0800

    feat: basic finish Labels.tsx

--- src/scripts/useTags.tsx
+++ src/scripts/useTags.tsx
@@ -3,7 +3,20 @@ import {useState} from "react"
 const useTags = () => {
   const [tags, setTags] = useState<string[]>(['餐饮', '购物', '交通', '娱乐', '工资'])
 
-  return {tags, setTags}
+  const addTag = () => {
+    let newTag = window.prompt('请输入新的标签名');
+    if (newTag === null || newTag === '') {
+      window.alert('新标签名不能为空');
+    } else if (tags.indexOf(newTag) >= 0) {
+      window.alert('当前标签已存在');
+    } else {
+      let tagsClone = tags
+      tagsClone.push(newTag);
+      setTags([...tagsClone]);
+    }
+  }
+
+  return {tags, setTags, addTag}
 }
 
 export {useTags}
--- src/views/labels/index.tsx
+++ src/views/labels/index.tsx
@@ -1,12 +1,62 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
+import styled from 'styled-components';
 import Layout from "../../components/Layout";
+import Icon from "../../components/Icon";
+import {Button} from "../../components/Button";
+import {useTags} from "../../scripts/useTags";
 
-function Index() {
+function Labels() {
+  const {tags, addTag} = useTags()
+
   return (
       <Layout title={'标签'}>
-        Labels
+        <TagList>
+          {tags.map((tag) =>
+              <li key={tag}>
+                <NavLink to={`/tag/${tag}`}>
+                  <span className="oneLine">{tag}</span>
+                  <Icon name="right"/>
+                </NavLink>
+              </li>
+          )}
+        </TagList>
+        <Center>
+          <Button onClick={() => {addTag()}}>新建标签</Button>
+        </Center>
       </Layout>
   )
 }
 
-export default Index;
+export default Labels;
+
+const TagList = styled.ol`
+  font-size: 16px; 
+  padding-left: 16px;
+  > li{
+    min-height: 44px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-bottom: 1px solid #e6e6e6;
+    > a{
+      width: 100%;
+      padding: 12px 16px 12px 0;
+      display:flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    > .icon {
+      width: 18px;
+      height: 18px;
+      fill: #666;
+      margin-right: 16px;
+    }
+  }
+`;
+const Center = styled.div`
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 28px;
+`;
