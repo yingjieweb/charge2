fdde2a246961437a2f880fa229d1514e2685a2d1
yingjieweb <1184061078@qq.com>
2022-04-10 23:14:33 +0800

    feat: encapsulate Button.tsx

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  font-size: 16px;
+  color: white;
+  background: #767676;
+  border: none;
+  border-radius: 4px;
+  height: 40px;
+  padding: 0 16px;
+`;
+
+export {Button}
