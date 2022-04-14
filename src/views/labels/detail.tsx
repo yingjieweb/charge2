8664f0e601dac983c40291b154daa2d28e4ff270
yingjieweb <1184061078@qq.com>
2022-04-14 23:49:10 +0800

    fix: basic finish LabelDetail comp

--- src/views/labels/detail.tsx
+++ src/views/labels/detail.tsx
@@ -1,15 +1,56 @@
 import React from "react";
-import {useParams} from "react-router-dom"
+import {useParams, useNavigate} from "react-router-dom"
 import {useTags} from "../../scripts/useTags";
+import styled from "styled-components";
+import Layout from "../../components/Layout";
+import {Button} from "../../components/Button";
+import {Input} from "../../components/Input";
 
 function Detail() {
-  const {findTag} = useTags()
-  let {tagId} = useParams();
+  const {findTag, editTag} = useTags()
+
+  const {tagId} = useParams()
+  const navigate = useNavigate()
 
   const currentTag = findTag(parseInt(tagId!))
 
+  const inputRef = React.createRef<HTMLInputElement>()
+  const onBlur = () => {
+    if (inputRef.current !== null) {
+      currentTag.tagName = inputRef.current.value
+    }
+  }
+
+  const submit = () => {
+    editTag(parseInt(tagId!), currentTag.tagName)
+    navigate(-1)
+    alert('标签修改成功')
+  }
+
   return (
-      <div>{currentTag.tagName}</div>
+      <Layout title={'编辑标签'}>
+        <Wrapper>
+          <Input label="备注" type="text" placeholder="请输入账单备注" ref={inputRef} defaultValue={currentTag.tagName} onBlur={onBlur}/>
+        </Wrapper>
+
+        <Center>
+          <Button onClick={() => {submit()}}>确定</Button>
+        </Center>
+      </Layout>
   )
 }
+
 export default Detail;
+
+
+const Wrapper = styled.section`
+  background: #f5f5f5;
+  padding: 0 16px;
+  font-size: 14px;
+`;
+const Center = styled.div`
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 28px;
+`;
